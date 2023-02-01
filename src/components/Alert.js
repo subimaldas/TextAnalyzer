799c139cc865099a2f7a877ddad47827d32858d2
Subimal Das <er.subimaldas@gmail.com>
2023-02-01 20:58:36 +0530

    initial change

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function Alert(props) {
+
+
+
+    //This function is used to capitalize the 1st letter i.e succes.
     const capitalize = (word)=>{
         const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
